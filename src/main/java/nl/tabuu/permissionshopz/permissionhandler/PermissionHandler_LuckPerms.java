d0e68905bd07d9b26d1be9c774d0db3828f5a272
Rick van Sloten <rickvansloten@gmail.com>
2020-04-11 21:13:55 +0200

    Added maven and fixed LuckPerms.

---
+++ src/main/java/nl/tabuu/permissionshopz/permissionhandler/PermissionHandler_LuckPerms.java
@@ -0,0 +1,29 @@
+package nl.tabuu.permissionshopz.permissionhandler;
+
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.types.PermissionNode;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class PermissionHandler_LuckPerms implements IPermissionHandler {
+
+    private LuckPerms _luckPermsApi;
+
+    public PermissionHandler_LuckPerms(){
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        _luckPermsApi = provider == null ? null : provider.getProvider();
+    }
+
+    @Override
+    public void addPermission(Player player, String permission) {
+        PermissionNode node = PermissionNode.builder(permission).build();
+        User user = _luckPermsApi.getUserManager().getUser(player.getUniqueId());
+        if(user == null) return;
+
+
+        user.data().add(node);
+        _luckPermsApi.getUserManager().saveUser(user);
+    }
+}
