e37717d646a6bd4a21edd027f4e2be819821fdd6
Rick van Sloten <rickvansloten@gmail.com>
2020-04-12 00:19:34 +0200

    Minor cleanup.

--- src/main/java/nl/tabuu/permissionshopz/gui/ShopInterface.java
+++ src/main/java/nl/tabuu/permissionshopz/gui/ShopInterface.java
@@ -34,7 +34,7 @@ public class ShopInterface extends InventoryFormUI {
     private Player _player;
     private IPermissionHandler _permissionHandler;
 
-    public ShopInterface(Player player){
+    public ShopInterface(Player player) {
         super("", InventorySize.TWO_ROWS);
 
         ConfigurationManager manager = PermissionShopZ.getInstance().getConfigurationManager();
@@ -53,34 +53,34 @@ public class ShopInterface extends InventoryFormUI {
     @Override
     protected void draw() {
         ItemBuilder
-                next =              new ItemBuilder(XMaterial.GREEN_STAINED_GLASS_PANE)
-                                            .setDisplayName(_local.translate("GUI_PAGE_NEXT")),
+                next = new ItemBuilder(XMaterial.GREEN_STAINED_GLASS_PANE)
+                .setDisplayName(_local.translate("GUI_PAGE_NEXT")),
 
-                previous =          new ItemBuilder(XMaterial.GREEN_STAINED_GLASS_PANE)
-                                            .setDisplayName(_local.translate("GUI_PAGE_PREVIOUS")),
+                previous = new ItemBuilder(XMaterial.GREEN_STAINED_GLASS_PANE)
+                        .setDisplayName(_local.translate("GUI_PAGE_PREVIOUS")),
 
-                barrier =           new ItemBuilder(XMaterial.BARRIER)
-                                            .setDisplayName(_local.translate("GUI_PAGE_CLOSE"));
+                barrier = new ItemBuilder(XMaterial.BARRIER)
+                        .setDisplayName(_local.translate("GUI_PAGE_CLOSE"));
 
         Style
-                nextButtonStyle =       new Style(next.build(), next.build()),
-                previousButtonStyle =   new Style(previous.build(), previous.build()),
-                exitButtonStyle =       new Style(barrier.build(), barrier.build());
+                nextButtonStyle = new Style(next.build(), next.build()),
+                previousButtonStyle = new Style(previous.build(), previous.build()),
+                exitButtonStyle = new Style(barrier.build(), barrier.build());
 
         Button
-                nextButton =            new Button(nextButtonStyle, this::nextPage),
-                previousButton =        new Button(previousButtonStyle, this::previousPage),
-                exitButton =            new Button(exitButtonStyle, this::onCloseButton);
+                nextButton = new Button(nextButtonStyle, this::nextPage),
+                previousButton = new Button(previousButtonStyle, this::previousPage),
+                exitButton = new Button(exitButtonStyle, this::onCloseButton);
 
         setElement(new Vector2f(8, 1), nextButton);
         setElement(new Vector2f(0, 1), previousButton);
         setElement(new Vector2f(4, 1), exitButton);
 
         List<Perk> perks = new ArrayList<>(PermissionShopZ.getInstance().getPerkManager().getPerks());
-        for(int i = _currentPage * 9; i < (_currentPage * 9) + 9; i++){
+        for (int i = _currentPage * 9; i < (_currentPage * 9) + 9; i++) {
             Vector2f position = new Vector2f(i % 9, 0);
 
-            if(perks.size() < i + 1) {
+            if (perks.size() < i + 1) {
                 setElement(position, new Button(new Style(XMaterial.AIR, XMaterial.AIR)));
                 continue;
             }
@@ -91,24 +91,24 @@ public class ShopInterface extends InventoryFormUI {
         super.draw();
     }
 
-    protected Button createPerkItem(Player player, Perk perk){
+    protected Button createPerkItem(Player player, Perk perk) {
         XMaterial unlockedItem = _config.getEnum(XMaterial.class, "UnlockedMaterial");
 
         boolean unlocked = perk.getPermissions().stream().allMatch(node -> _player.hasPermission(node));
         ItemBuilder displayItem = new ItemBuilder(unlocked ? unlockedItem.parseItem() : perk.getDisplayItem());
         displayItem.setDisplayName(ChatColor.translateAlternateColorCodes('&', perk.getName()));
 
-        if(_config.getBoolean("DisplayPermissionList")){
-            for(String node : perk.getPermissions()) {
+        if (_config.getBoolean("DisplayPermissionList")) {
+            for (String node : perk.getPermissions()) {
                 String text = "PERK_PERMISSION_NODE";
-                if(_player.hasPermission(node))
+                if (_player.hasPermission(node))
                     text += "_HAS";
 
                 displayItem.addLore(_local.translate(text, "{NODE}", node));
             }
         }
 
-        if(unlocked)
+        if (unlocked)
             displayItem.addLore(_local.translate("GUI_PERK_UNLOCKED"));
         else
             displayItem.addLore(_local.translate("GUI_PERK_PRICE", "{PRICE}", suffixFormat(perk.getCost())));
@@ -120,47 +120,46 @@ public class ShopInterface extends InventoryFormUI {
         return button;
     }
 
-    protected void onPerkClick(Player player, Perk perk){
-        if(_economy.has(player, perk.getCost())){
+    protected void onPerkClick(Player player, Perk perk) {
+        if (_economy.has(player, perk.getCost())) {
             _economy.withdrawPlayer(player, perk.getCost());
             perk.getPermissions().forEach(node -> _permissionHandler.addPermission(player, node));
             Message.send(player, _local.translate("PERK_BUY_SUCCESSFUL", "{PERK_NAME}", perk.getName()));
-        }
-        else{
+        } else {
             Message.send(player, _local.translate("PERK_BUY_INSUFFICIENTFUNDS", "{PERK_NAME}", perk.getName()));
         }
         updateTitle();
     }
 
-    protected void updateTitle(){
+    protected void updateTitle() {
         setTitle(_local.translate("GUI_TITLE", "{PAGE_NUMBER}", (_currentPage + 1) + ""));
         reload();
         draw();
     }
 
-    private void onCloseButton(Player player){
+    private void onCloseButton(Player player) {
         this.close(player);
     }
 
-    private void nextPage(Player player){
-        if(_currentPage < _maxPage)
+    private void nextPage(Player player) {
+        if (_currentPage < _maxPage)
             _currentPage++;
 
         updateTitle();
     }
 
-    private void previousPage(Player player){
-        if(_currentPage > 0)
+    private void previousPage(Player player) {
+        if (_currentPage > 0)
             _currentPage--;
 
         updateTitle();
     }
 
-    private String suffixFormat(double value){
+    private String suffixFormat(double value) {
         NavigableMap<Double, String> suffixMap = new TreeMap<>();
         Set<String> suffixList = _config.getConfigurationSection("NumberSuffixes").getKeys(false);
 
-        for(String string : suffixList){
+        for (String string : suffixList) {
             int zeroCount = Integer.parseInt(string);
             String suffix = _config.getString("NumberSuffixes." + string);
             suffixMap.put(Math.pow(10d, zeroCount), suffix);
@@ -168,8 +167,7 @@ public class ShopInterface extends InventoryFormUI {
 
         Map.Entry<Double, String> entry = suffixMap.floorEntry(value);
 
-        if(entry == null)
-            return value + "";
+        if (entry == null) return value + "";
 
         Double divider = entry.getKey();
         String suffix = entry.getValue();
--- src/main/java/nl/tabuu/permissionshopz/util/Message.java
+++ src/main/java/nl/tabuu/permissionshopz/util/Message.java
@@ -2,21 +2,22 @@ package nl.tabuu.permissionshopz.util;
 
 import nl.tabuu.permissionshopz.PermissionShopZ;
 import nl.tabuu.tabuucore.api.TitleAPI;
+import nl.tabuu.tabuucore.configuration.IConfiguration;
 import org.bukkit.entity.Player;
 import org.json.simple.JSONObject;
 
 public class Message {
 
-    public static void send(Player player, String message){
-        if(PermissionShopZ.getInstance().getConfigurationManager().getConfiguration("config").getBoolean("UseTitle"))
+    public static void send(Player player, String message) {
+        IConfiguration config = PermissionShopZ.getInstance().getConfigurationManager().getConfiguration("config");
+        if (config.getBoolean("UseTitle"))
             TitleAPI.getInstance().sendActionbar(player, getJSONMessage(message), 0, 5, 0);
         else
             player.sendMessage(message);
     }
 
     private static String getJSONMessage(String message) {
-        String json = "{\"text\":\"MESSAGE\"}".replace("MESSAGE", JSONObject.escape(message));
-        return json;
+        return "{\"text\":\"MESSAGE\"}".replace("MESSAGE", JSONObject.escape(message));
     }
 
 }
